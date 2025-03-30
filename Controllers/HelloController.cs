d0cc74b8d5adcec12cac73e6bec8b946d74366a4
tuan kiet <tnkiet0512@gmail.com>
2025-03-30 17:33:23 +0700

    Project initialization

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Integration_System.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok("hello world");
+    }
+}
